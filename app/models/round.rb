fc48f12037bffad384b6e7e59a27cc1f9f2b8499
Apprentice <apprentice@devbootcamp.com>
2013-04-05 16:40:42 -0700

    added helper to Round

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,16 @@ class Round < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :user
   has_many :guesses
+
+  after_create :pull_deck
+
+  has_many :cards, :through => :guesses
+  
+  attr_accessor :choosen_deck
+
+  private
+  def pull_deck
+    self.cards = choosen_deck.cards.all.shuffle
+  end
+
 end
