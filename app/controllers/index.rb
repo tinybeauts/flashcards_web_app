f2d865c0e4d3ba4a458b4e6bcc9099dd2c0d8c4c
DBC Apprentice <apprentice@devbootcamp.com>
2013-04-05 15:42:39 -0700

    Added routes for login/logout

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,34 @@
+require 'bcrypt'
+
+enable :sessions
+
 get '/' do
-  # Look in app/views/index.erb
+  sessions[:current_user_id] ? erb :decks : erb :index
+end
+
+get '/register' do
+  erb :register
+end
+
+post '/add_user' do
+  password = BCRYPT::Password.create(params[:password])
+  user = User.create(name: params[:name], email: params[:email], password: password)
+  login! user
+  erb: decks
+end
+
+post '/login' do
+  user = User.find_by_email(params[:email])
+  if user.password == params[:password]
+    login! user
+    erb: decks
+  else
+    @message = "Login failed"
+    erb: index
+  end
+end
+
+post '/logout' do
+  logout!
   erb :index
 end
---
+++ app/helpers/login_helpers.rb
@@ -0,0 +1,16 @@
+def login! user
+  session[:current_user_id] = user.id
+end
+
+def logout!
+  session.clear
+end
+
+def logged_in?
+  session[:current_user_id].present?
+end
+
+def current_user
+  return nil unless logged_in?
+  @current_user ||= User.find(session[:current_user_id])
+end
