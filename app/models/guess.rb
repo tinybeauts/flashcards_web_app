6836f20c29621269a6c51bc5f0b48afa7a9a23a2
Apprentice <apprentice@devbootcamp.com>
2013-04-05 16:41:23 -0700

    added scope to guess

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,7 +2,7 @@ class Guess < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :round
   belongs_to :card
-
+  scope :remaining, where(:correct=>false)
   validate :successful_attempt, :on=>:update
 
   def attempt!(answer)
