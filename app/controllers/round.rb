06878ada810f6345b3b4d8d46eaadcd9b6388edb
Apprentice <apprentice@devbootcamp.com>
2013-04-05 16:27:20 -0700

    wip: fixing up controllers

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -2,15 +2,41 @@ get "/rounds" do
   @rounds = current_user.rounds.all
 end
 
-get "/round/:id" do
-  @card = Round.find(:id).remaining_cards.sample
+get "/round/:id/guess" do
+  @message = params[:message]
+
+  @guess = Round.find(params[:id]).guesses.sample
+  hold_guess(@guess)
+  @card = @guess.card
   erb :"round/show"
 end
 
-post "/round/:id" do
-  current_round = current_user.rounds.find(:id)
-  card = current_round.guesses.find(params[:card][:id])
-  card.guess(params[:card][:guess])
-  id = params[:guess]
-  redirect to("/round/#{id}")
+post "/round/:id/guess" do
+  @guess = current_guess
+  @guess.attempt!(params[:attempt])
+  session[:errors] = @guess.errors unless @guess.valid?
+  redirect to(url)
 end
+
+helpers do
+  def current_guess
+    Guess.find(session[:guess_id])
+  end
+end
+
+def hold_guess(guess)
+  session[:guess_id] = guess.id
+end
+
+
+
+#get /round/:id/guess <- put the guess into session
+#post /round/:id/guess
+
+  # @card = @guess.card
+  # if params[:attempt] == @card.answer
+  #   @guess.correct = true
+  # else
+  #   url<<"?message=incorrect"
+  # end
+
