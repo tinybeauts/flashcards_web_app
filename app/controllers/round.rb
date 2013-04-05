94a34b908846410936cb0bc690ae2dffd1136008
Apprentice <apprentice@devbootcamp.com>
2013-04-05 15:11:50 -0700

    fixed missing do

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,13 +1,13 @@
-get "/rounds"
+get "/rounds" do
   @rounds = current_user.rounds.all
 end
 
-get "/round/:id"
+get "/round/:id" do
   @card = Round.find(:id).remaining_cards.sample
   erb :"round/show"
 end
 
-post "/round/:id"
+post "/round/:id" do
   current_round = current_user.rounds.find(:id)
   card = current_round.guesses.find(params[:card][:id])
   card.guess(params[:card][:guess])
