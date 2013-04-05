a2fe26304d5872da8f972972a753f813b911e79f
Apprentice <apprentice@devbootcamp.com>
2013-04-05 16:25:51 -0700

    added validations and custom methods

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,18 @@ class Guess < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :round
   belongs_to :card
+
+  validate :successful_attempt, :on=>:update
+
+  def attempt!(answer)
+    @attempt = answer
+    save
+  end
+
+  def successful_attempt
+    if @answer == self.card.answer
+      errors.add(:attempt, "was not the answer")
+    end
+  end
+
 end
